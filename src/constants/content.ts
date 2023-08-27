3e6e4e876d4944c44dbae363974d0f155f876cc9
robbiesymonds <robbiesymonds26@gmail.com>
2023-08-27 22:46:40 +0930

    initial commit

---
+++ src/constants/content.ts
@@ -0,0 +1,14 @@
+export const MESSAGES = [
+  [
+    'Hey there! We are Layerhouse',
+    "We're sort of a digital consultancy, but mostly just<br />engineers with a love for details, design, etc."
+  ],
+  [
+    "We're working on a bunch of stuff right now....",
+    "but we're always looking for great people to work with.",
+    'maybe with you? 👉👈'
+  ],
+  [
+    'Anyway, feel free to reach out to us at <a target="_blank" href="mailto:hello@layerhouse.co">hello@layerhouse.co</a>, or just send us a message by replying to this chat.'
+  ]
+]
