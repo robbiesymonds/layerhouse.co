7d3d4d455f3207f3f6281059aa1869457aab1c2d
robbiesymonds <robbiesymonds26@gmail.com>
2023-08-29 23:28:59 +0930

    added alts

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -5,7 +5,7 @@ type Props = {
 export const Avatar = ({ src }: Props) => {
   return (
     <div className="avatar">
-      <img src={src} />
+      <img src={src} alt="Avatar" />
     </div>
   )
 }
